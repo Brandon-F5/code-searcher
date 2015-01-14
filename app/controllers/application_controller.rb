28c9c5383723de6580e89635dfd3340361ea129a
Brandon Pollet <bpollet@gmail.com>
2015-01-14 16:52:24 -0600

    arrgh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
  protect_from_forgery with: :exception
- before_filter :check_uri
  helper_method :current_user, :authenticate_user!
-  
- def check_uri
-   url = redirect_to request.protocol + 'www.' + request.host_with_port + request.fullpath if Rails.env.to_s == 'production' && !/^www/.match(request.host) && ENV['DEV'].nil?
- end
 
  def current_user
    @current_user ||= User.find_by_id(session[:u_id])
