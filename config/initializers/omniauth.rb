089ed974c858b4e8f6fd4f0eef13f32446daab0d
Brandon Pollet <bpollet@gmail.com>
2015-01-14 16:20:15 -0600

    removed assets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+ENV['GITHUB_KEY'] ||= 'cbee726022796a360628'
+ENV['GITHUB_SECRET'] ||= 'f9f4b293ca7c55f6ce3ab04ee789fa02f0569e60'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
